71919efe97756fd7803ca60f349c1eb3fe193da7
y377 <58632405+y377@users.noreply.github.com>
2023-08-01 19:05:13 +0800

    Update tyyp.py

--- tyyp.py
+++ tyyp.py
@@ -1,3 +1,4 @@
+
 import time
 import re
 import json
@@ -13,35 +14,58 @@ import os
 ty_username = os.getenv("TYYP_USERNAME").split('&')
 ty_password = os.getenv("TYYP_PSW").split('&')
 
+# GitHub Pages 
+import datetime
+now = datetime.datetime.now()
+result_file = f'签到结果_{now.year}_{now.month}_{now.day}.md'
+
+def save_result(content):
+    with open(result_file, 'a') as f:
+        f.write(content + '\n')
+
 BI_RM = list("0123456789abcdefghijklmnopqrstuvwxyz")
+
 B64MAP = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+
 s = requests.Session()
 
-# 创建日志字符串
-log_md = "# 签到日志\n"
 
 for i in range(len(ty_username)):
-    print(f'开始执行帐号{i + 1}')
+    print(f'开始执行帐号{i+1}')
+
 
     def int2char(a):
         return BI_RM[a]
 
+
     def b64tohex(a):
-        d, e, c = "", 0, 0
+        d = ""
+        e = 0
+        c = 0
         for i in range(len(a)):
             if list(a)[i] != "=":
                 v = B64MAP.index(list(a)[i])
-                if e == 0:
-                    e, d, c = 1, d + int2char(v >> 2), 3 & v
-                elif e == 1:
-                    e, d, c = 2, d + int2char(c << 2 | v >> 4), 15 & v
-                elif e == 2:
-                    e, d, c = 3, d + int2char(c) + int2char(v >> 2), 3 & v
+                if 0 == e:
+                    e = 1
+                    d += int2char(v >> 2)
+                    c = 3 & v
+                elif 1 == e:
+                    e = 2
+                    d += int2char(c << 2 | v >> 4)
+                    c = 15 & v
+                elif 2 == e:
+                    e = 3
+                    d += int2char(c)
+                    d += int2char(v >> 2)
+                    c = 3 & v
                 else:
-                    e, d = 0, d + int2char(c << 2 | v >> 4) + int2char(15 & v)
+                    e = 0
+                    d += int2char(c << 2 | v >> 4)
+                    d += int2char(15 & v)
         if e == 1:
             d += int2char(c << 2)
         return d
+
 
     def rsa_encode(j_rsakey, string):
         rsa_key = f"-----BEGIN PUBLIC KEY-----\n{j_rsakey}\n-----END PUBLIC KEY-----"
@@ -49,26 +73,45 @@ for i in range(len(ty_username)):
         result = b64tohex((base64.b64encode(rsa.encrypt(f'{string}'.encode(), pubkey))).decode())
         return result
 
+
+    def calculate_md5_sign(params):
+        return hashlib.md5('&'.join(sorted(params.split('&'))).encode('utf-8')).hexdigest()
+
+
     def login(ty_username, ty_password):
-        urlToken = "https://m.cloud.189.cn/udb/udb_login.jsp?pageId=1&redirectURL=https://m.cloud.189.cn/zhuanti/2021/shakeLottery/index.html"
+
+        url = ""
+        urlToken = "https://m.cloud.189.cn/udb/udb_login.jsp?pageId=1&pageKey=default&clientType=wap&redirectURL=https://m.cloud.189.cn/zhuanti/2021/shakeLottery/index.html"
         s = requests.Session()
         r = s.get(urlToken)
-        match = re.search(r"https?://[^\s'\"]+", r.text)
-        url = match.group() if match else print("没有找到url")
+        pattern = r"https?://[^\s'\"]+"  # 匹配以http或https开头的url
+        match = re.search(pattern, r.text)  # 在文本中搜索匹配
+        if match:  # 如果找到匹配
+            url = match.group()  # 获取匹配的字符串
+            # print(url)  # 打印url
+        else:  # 如果没有找到匹配
+            print("没有找到url")
 
         r = s.get(url)
-        match = re.search(r"<a id=\"j-tab-login-link\"[^>]*href=\"([^\"]+)\"", r.text)
-        href = match.group(1) if match else print("没有找到href链接")
+        # print(r.text)
+        pattern = r"<a id=\"j-tab-login-link\"[^>]*href=\"([^\"]+)\""  # 匹配id为j-tab-login-link的a标签，并捕获href引号内的内容
+        match = re.search(pattern, r.text)  # 在文本中搜索匹配
+        if match:  # 如果找到匹配
+            href = match.group(1)  # 获取捕获的内容
+            # print("href:" + href)  # 打印href链接
+        else:  # 如果没有找到匹配
+            print("没有找到href链接")
 
         r = s.get(href)
-        captchaToken, lt, returnUrl, paramId, j_rsakey = re.findall(r"captchaToken' value='(.+?)'", r.text)[0], \
-                                                          re.findall(r'lt = "(.+?)"', r.text)[0], \
-                                                          re.findall(r"returnUrl= '(.+?)'", r.text)[0], \
-                                                          re.findall(r'paramId = "(.+?)"', r.text)[0], \
-                                                          re.findall(r'j_rsaKey" value="(\S+)"', r.text, re.M)[0]
+        captchaToken = re.findall(r"captchaToken' value='(.+?)'", r.text)[0]
+        lt = re.findall(r'lt = "(.+?)"', r.text)[0]
+        returnUrl = re.findall(r"returnUrl= '(.+?)'", r.text)[0]
+        paramId = re.findall(r'paramId = "(.+?)"', r.text)[0]
+        j_rsakey = re.findall(r'j_rsaKey" value="(\S+)"', r.text, re.M)[0]
         s.headers.update({"lt": lt})
 
-        username, password = rsa_encode(j_rsakey, ty_username[i]), rsa_encode(j_rsakey, ty_password[i])
+        username = rsa_encode(j_rsakey, ty_username[i])
+        password = rsa_encode(j_rsakey,ty_password[i])
         url = "https://open.e.189.cn/api/logbox/oauth2/loginSubmit.do"
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/76.0',
@@ -86,40 +129,78 @@ for i in range(len(ty_username)):
             "paramId": paramId
         }
         r = s.post(url, data=data, headers=headers, timeout=5)
-        print(r.json()['msg'])
+        if (r.json()['result'] == 0):
+            print(r.json()['msg'])
+        else:
+            print(r.json()['msg'])
         redirect_url = r.json()['toUrl']
-        s.get(redirect_url)
+        r = s.get(redirect_url)
         return s
+
 
     def main():
         s = login(ty_username, ty_password)
         rand = str(round(time.time() * 1000))
-        url = f"https://m.cloud.189.cn/v2/drawPrizeMarketDetails.action?taskId=TASK_SIGNIN&activityId=ACT_SIGNIN&rand={rand}&clientType=TELEANDROID&version=8.7.0&model=SM-G930K"
-        response = s.get(url)
+        surl = f'https://api.cloud.189.cn/mkt/userSign.action?rand={rand}&clientType=TELEANDROID&version=8.6.3&model=SM-G930K'
+        url = f'https://m.cloud.189.cn/v2/drawPrizeMarketDetails.action?taskId=TASK_SIGNIN&activityId=ACT_SIGNIN'
+        url2 = f'https://m.cloud.189.cn/v2/drawPrizeMarketDetails.action?taskId=TASK_SIGNIN_PHOTOS&activityId=ACT_SIGNIN'
+        url3 = f'https://m.cloud.189.cn/v2/drawPrizeMarketDetails.action?taskId=TASK_2022_FLDFS_KJ&activityId=ACT_SIGNIN'
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 5.1.1; SM-G930K Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.136 Mobile Safari/537.36 Ecloud/8.6.3 Android/22 clientId/355325117317828 clientModel/SM-G930K imsi/460071114317824 clientChannelId/qq proVersion/1.0.6',
+            "Referer": "https://m.cloud.189.cn/zhuanti/2016/sign/index.jsp?albumBackupOpened=1",
+            "Host": "m.cloud.189.cn",
+            "Accept-Encoding": "gzip, deflate",
+        }
+        response = s.get(surl, headers=headers)
         netdiskBonus = response.json()['netdiskBonus']
-        res1 = f"未签到，签到获得{netdiskBonus}M空间" if response.json()['isSign'] == "false" else f"已经签到过了，签到获得{netdiskBonus}M空间"
+        if (response.json()['isSign'] == "false"):
+            print(f"未签到，签到获得{netdiskBonus}M空间")
+            res1 = f"未签到，签到获得{netdiskBonus}M空间"
+        else:
+            print(f"已经签到过了，签到获得{netdiskBonus}M空间")
+            res1 = f"已经签到过了，签到获得{netdiskBonus}M空间"
 
-        description = response.json()['description']
-        res2 = f"抽奖获得{description}" if "errorCode" not in response.text else ""
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 5.1.1; SM-G930K Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.136 Mobile Safari/537.36 Ecloud/8.6.3 Android/22 clientId/355325117317828 clientModel/SM-G930K imsi/460071114317824 clientChannelId/qq proVersion/1.0.6',
+            "Referer": "https://m.cloud.189.cn/zhuanti/2016/sign/index.jsp?albumBackupOpened=1",
+            "Host": "m.cloud.189.cn",
+            "Accept-Encoding": "gzip, deflate",
+        }
+        response = s.get(url, headers=headers)
+        if ("errorCode" in response.text):
+            print(response.text)
+            res2 = ""
+        else:
+            description = response.json()['description']
+            print(f"抽奖获得{description}")
+            res2 = f"抽奖获得{description}"
+        response = s.get(url2, headers=headers)
+        if ("errorCode" in response.text):
+            print(response.text)
+            res3 = ""
+        else:
+            description = response.json()['description']
+            print(f"抽奖获得{description}")
+            res3 = f"抽奖获得{description}"
 
-        url = "https://m.cloud.189.cn/v2/drawPrizeMarketDetails.action?taskId=TASK_SIGNIN_PHOTOS&activityId=ACT_SIGNIN&rand=6541015307801&clientType=TELEANDROID&version=8.6.3&model=SM-G930K"
-        response = s.get(url)
-        description = response.json()['description']
-        res3 = f"抽奖获得{description}" if "errorCode" not in response.text else ""
+        response = s.get(url3, headers=headers)
+        if ("errorCode" in response.text):
+            print(response.text)
+            res4 = ""
+        else:
+            description = response.json()['description']
+            print(f"链接3抽奖获得{description}")
+            res4 = f"链接3抽奖获得{description}"
+        #message = res1+res2+res3+res4
+        #Push(contents=message)
 
-        url = "https://m.cloud.189.cn/v2/drawPrizeMarketDetails.action?taskId=TASK_SIGNIN_COIN&activityId=ACT_SIGNIN&rand=6541015307801&clientType=TELEANDROID&version=8.6.3&model=SM-G930K"
-        response = s.get(url)
-        description = response.json()['description']
-        res4 = f"链接3抽奖获得{description}" if "errorCode" not in response.text else ""
 
-        return res1, res2, res3, res4
+    if __name__ == "__main__":
 
-    # 将每个账号的结果添加到日志字符串
-    result1, result2, result3, result4 = main()
-    log_md += f"\n帐号{i + 1}:\n- {result1}\n- {result2}\n- {result3}\n- {result4}\n"
-
-# 将日志字符串写入文件
-with open("log.md", "w") as file:
-    file.write(log_md)
-
-print("所有帐号执行完毕")
+        main()
+# 保存结果
+save_result(f'## 签到结果 {now}')  
+save_result(res1)
+save_result(res2) 
+save_result(res3)
+save_result(res4)
