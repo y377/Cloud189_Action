b7f8accb132283c43b83bb8775319d3c2195e0b4
y377 <58632405+y377@users.noreply.github.com>
2023-08-03 22:40:26 +0800

    Update tyyp.py

--- tyyp.py
+++ tyyp.py
@@ -9,8 +9,9 @@ import requests
 import random
 import os
 
-ty_username = os.getenv("TYYP_USERNAME")
-ty_password = os.getenv("TYYP_PSW")
+# 变量 ty_username（手机号）,ty_password（密码）
+ty_username = os.getenv("TYYP_USERNAME").split('&')
+ty_password = os.getenv("TYYP_PSW").split('&')
 
 BI_RM = list("0123456789abcdefghijklmnopqrstuvwxyz")
 
